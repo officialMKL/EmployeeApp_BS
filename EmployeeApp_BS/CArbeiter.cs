ea89b68492285d4098758e7fe8790b3bfb95e2d9
officialMKL <mkl.98@gmx.de>
2018-02-07 00:01:51 +0100

    Part 2 of Employees implemented
    added CArbeiter

---
+++ EmployeeApp_BS/CArbeiter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace OOP_BS
+{
+    public class CArbeiter : CMitarbeiter
+    {
+        private int Stunden;
+        private double Stundenlohn;
+
+        public CArbeiter(string NN, string VN, int std, double stdLohn) : base(NN, VN)
+        {
+            setStunden(std);
+            setStundenlohn(stdLohn);
+        }
+
+        public void setStunden(int std) 
+        {
+            try {
+                if(std > 0) {
+                    this.Stunden = std;
+                }
+                else {
+                    throw new Exception("Bitte eine gültige Stundenzahl angegeben");
+                }
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void setStundenlohn(double stdLohn) 
+        {
+            try {
+                if(stdLohn > 0) {
+                    this.Stundenlohn = stdLohn;
+                }
+                else {
+                    throw new Exception("Bitte einen gültigen Stundenlohn angeben");
+                }
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public double getBrutto() 
+        {
+            return this.Stunden * this.Stundenlohn;
+        }
+    }
+}
