3a678924c5e07a8b8384dd7c696dd7e2c2a54cdd
officialMKL <mkl.98@gmx.de>
2018-02-06 22:40:22 +0100

    init Project
    initial commit

---
+++ OOP_BS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_BS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
