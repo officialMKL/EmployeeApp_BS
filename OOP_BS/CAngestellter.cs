9092ddb26655a98450566bba78c842975b4e1c2d
officialMKL <mkl.98@gmx.de>
2018-02-06 23:26:48 +0100

    Part 1 of Employees implemented
    added CMitarbeiter
    added CAngestellter

---
+++ OOP_BS/CAngestellter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace OOP_BS
+{
+    public class CAngestellter : CMitarbeiter
+    {
+        private double Bruttogehalt;
+
+        public CAngestellter(string NN, string VN, double brutto) : base(NN, VN)
+        {
+            setBrutto(brutto);
+        }
+
+        public void setBrutto(double brutto) 
+        {
+            try {
+				if(brutto > 0) 
+				{
+					this.Bruttogehalt = brutto;            
+				}
+				else 
+				{
+					throw new Exception("Bitte ein gültiges Gehalt angeben");
+				}
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public double getBrutto() 
+        {
+            return this.Bruttogehalt;
+        }
+    }
+}
---
+++ OOP_BS/CMitarbeiter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace OOP_BS
+{
+    public class CMitarbeiter
+    {
+        private string Nachname;
+        private string Vorname;
+
+        public CMitarbeiter(string NN, string VN)
+        {
+            setNachname(NN);
+            setVorname(VN);
+        }
+
+        public void setNachname(string NN) 
+        {
+            this.Nachname = NN;
+        }
+
+        public void setVorname(string VN) 
+        {
+            this.Vorname = VN;
+        }
+
+        public string getNachname() 
+        {
+            return this.Nachname;
+        }
+
+        public string getVorname() 
+        {
+            return this.Vorname;
+        }
+    }
+}
