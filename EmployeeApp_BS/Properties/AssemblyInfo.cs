84fda69cc94c700c686989b643e64a17477f28f4
officialMKL <mkl.98@gmx.de>
2018-02-07 23:03:20 +0100

    Namespaces refactored
    last Namespace changes done

--- EmployeeApp_BS/Properties/AssemblyInfo.cs
+++ EmployeeApp_BS/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("OOP_BS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EmployeeApp_BS")]
+[assembly: AssemblyDescription("App for getting familiar with oop features")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MKL-Solutions")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
